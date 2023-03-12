8ec110c02ef94c666c9ec20c6db664bb80a08f37
miguelferreira2000 <miguelferreiracomprasonline@gmail.com>
2023-03-12 19:08:56 +0000

    a50931@etapa_Change a GameObject with script

--- Assets/Trackposition.cs
+++ Assets/Trackposition.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hello_world : MonoBehaviour
+public class Trackposition : MonoBehaviour
 {
-    public string myMessage; 
+    public Vector3 positionChange;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(myMessage);
+        transform.position += positionChange;
     }
 }
