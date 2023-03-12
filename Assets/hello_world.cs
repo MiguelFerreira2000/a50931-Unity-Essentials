e7023bbdf3066a98bc441e7e5f19cd680daa6d80
miguelferreira2000 <miguelferreiracomprasonline@gmail.com>
2023-03-12 18:55:52 +0000

    a50931@etapa_Code in the default script

--- Assets/hello_world.cs
+++ Assets/hello_world.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class hello_world : MonoBehaviour
 {
+    public string myMessage; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(myMessage);
     }
 }
